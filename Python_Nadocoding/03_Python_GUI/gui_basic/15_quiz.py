93d5b5b922e7872f0bca3863642e60209a21106d
Dongho Lee <ldh9801@gmail.com>
2025-05-19 15:35:30 +0900

    project 착수

---
+++ Python_Nadocoding/03_Python_GUI/gui_basic/15_quiz.py
@@ -0,0 +1,17 @@
+# Quiz : tkinter 이용한 메모장 프로그램 만들기
+
+# [GUI 조건]
+# 1. title : 제목 없음 - windows 메모장
+# 2. 메뉴 : 파일, 편집, 서식, 보기, 도움말
+# 3. 실제 메뉴 구현 : 파일 메뉴 내에서 열기, 저장, 끝내기 3 개만 처리
+# 3-1. 열기 : mynote.txt 파일 내용 열어서 보여주기
+# 3-2. 저장 : mynote.txt 파일에 현재 내용 저장하기
+# 3-3. 끝내기 : 프로그램 종료
+# 4. 프로그램 시작 시 본문은 비어있는 상태
+# 5. 하단 status 바 구현 X
+# 6. 프로그램 크기, 위치 자유, 크기 조정 가능해야 함
+# 7. 본문 우측 상하 스크롤 바 넣기
+
+# https://www.youtube.com/watch?v=bKPIcoou9N8&list=PLMsa_0kAjjrd8hYYCwbAuDsXZmHpqHvlV&index=5
+# 1:57:48 ~
+# 직접 해보기!!
